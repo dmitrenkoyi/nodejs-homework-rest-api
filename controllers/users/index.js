8cd647e7a27e49970c4b0d4a5491a44e00274488
dmitrenkoyi <artemovayi@gmail.com>
2022-01-03 18:06:43 +0200

    выполнено без тестов

---
+++ controllers/users/index.js
@@ -0,0 +1,15 @@
+const signup = require("./signup");
+const login = require("./login");
+const current = require("./getCurrent");
+const logout = require("./logout");
+const updateSubscription = require("./updateSubscription.js");
+const updateAvatar = require("./updateAvatar");
+
+module.exports = {
+  signup,
+  login,
+  current,
+  logout,
+  updateSubscription,
+  updateAvatar,
+};
